521258bc2826e4ae70b9c2d1888fad4108fe7273
Matt Harris <mattyharris81@gmail.com>
2015-03-11 12:10:38 +0000

    added susy and css changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,9 @@ var gulp = require('gulp');
 // Include plugins
 // Sass / CSS
 var sass = require('gulp-sass');
-//var sourcemaps = require('gulp-sourcemaps');
+var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
-
-// Rename files
-var rename = require('gulp-rename');
 
 // notify when task is complete
 var notify = require('gulp-notify');
@@ -20,24 +17,44 @@ var notify = require('gulp-notify');
 // Compile sass
 gulp.task('css', function() {
   gulp.src('scss/**/*.scss')
-    //.pipe(sourcemaps.init())
-    .pipe(sass())
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    errLogToConsole: true
+  }))
 
-    // auto prefix css
-    .pipe(prefix('last 2 versions'))
+  // auto prefix css
+  .pipe(prefix('last 2 versions'))
 
-    // minify the css file
-    .pipe(minifycss())
+  // minify the css file
+  .pipe(minifycss())
 
-    // move css file to folder
-    .pipe(gulp.dest('css/'))
+  // move css file to folder
+  .pipe(gulp.dest('css/'))
 
-    //.pipe(sourcemaps.write())
+  .pipe(sourcemaps.write())
 
-    // notify to say the task has complete
-    .pipe(notify({
-      message: 'CSS task complete'
-    }))
+  // notify to say the task has complete
+  .pipe(notify({
+    message: 'CSS task complete'
+  }))
+});
+
+// Concatenate js files and minify
+gulp.task('js', function() {
+
+  // notify to say the task has complete
+  .pipe(notify({
+    message: 'JS task complete'
+  }))
+});
+
+// Compress images
+gulp.task('images', function() {
+
+  // notify to say the task has complete
+  .pipe(notify({
+    message: 'Images task complete'
+  }))
 });
 
 // Watch files for changes
